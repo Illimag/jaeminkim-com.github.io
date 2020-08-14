1bc528245433230d15136dbc5be19c921b11e68c
uiuxdeveloper-io <uiuxdeveloper@comcast.net>
2020-08-14 10:23:26 -0700

    update with react bootstrap router.

--- README.md
+++ README.md
@@ -1,1 +1,102 @@
 # Jaeminkim.com
+
+## Features
+
+6 portfolio pieces in each individual page.
+
+Home page.
+
+Contact Page.
+
+React App using React Router to route to different pages.
+
+Single Page Web App, load all assets of websites in inital load, so the website will run fast no matter what device and internet network.
+
+Loading Screen that will show how long the user will have to wait for the portfolio to load..
+
+Check if the user is using a mobile device, maybe load a mobile version of the site using assets of different sizes so it will load faster.
+
+## Installing React App
+
+To Install React just use the CLI
+
+    npx create-react-app nameofapp
+
+This command will install React App
+
+Make sure to have node and npm installed.
+
+## Installing React Router to React.JS App
+
+React Router is the functionality to route HTML via the Single Page Web Application Functionality.
+
+Using this functionality we can implement menus and sub menus and direct the single page to various HTMLs to simulate a multi page experience.
+
+Install via npm
+
+    npm install react-router-dom
+
+Example code of React Router
+
+    import React from "react";
+    import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+    } from "react-router-dom";
+
+    export default function App() {
+    return (
+        <Router>
+        <div>
+            <nav>
+            <ul>
+                <li>
+                <Link to="/">Home</Link>
+                </li>
+                <li>
+                <Link to="/about">About</Link>
+                </li>
+                <li>
+                <Link to="/users">Users</Link>
+                </li>
+            </ul>
+            </nav>
+
+            {/* A <Switch> looks through its children <Route>s and
+                renders the first one that matches the current URL. */}
+            <Switch>
+            <Route path="/about">
+                <About />
+            </Route>
+            <Route path="/users">
+                <Users />
+            </Route>
+            <Route path="/">
+                <Home />
+            </Route>
+            </Switch>
+        </div>
+        </Router>
+    );
+    }
+
+    function Home() {
+    return <h2>Home</h2>;
+    }
+
+    function About() {
+    return <h2>About</h2>;
+    }
+
+    function Users() {
+    return <h2>Users</h2>;
+    }
+
+## Installing React-Bootstrap
+
+Install via npm
+
+    npm install react-bootstrap bootstrap
+
--- jaeminkim.com/src/App.js
+++ jaeminkim.com/src/App.js
@@ -1,55 +1,64 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import React from 'react';
+import { MemoryRouter, Switch, Route } from 'react-router-dom';
 
-export default function App() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
-}
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
-function Home() {
-  return <h2>Home</h2>;
-}
+import { LinkContainer } from 'react-router-bootstrap';
 
-function About() {
-  return <h2>About</h2>;
-}
+import './App.css';
 
-function Users() {
-  return <h2>Users</h2>;
-}
+const Test0 = () => <span>test0</span>;
+
+const Test1 = () => <span>test1</span>;
+
+const Test2 = () => <span>test2</span>;
+
+const App = () => (
+  <MemoryRouter>
+    <Container className="p-3">
+      <Jumbotron>
+
+          <Switch>
+            <Route path="/test1">
+              <Test1 />
+            </Route>
+            <Route path="/test2">
+              <Test2 />
+            </Route>
+            <Route path="/"> 
+              <Test0 />
+            </Route>
+          </Switch>
+
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">JAEMINKIM.COM</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+
+            <LinkContainer to="/">
+              <Nav.Link>test0</Nav.Link>
+            </LinkContainer>
+
+            <LinkContainer to="/test1">
+              <Nav.Link>test1</Nav.Link>
+            </LinkContainer>
+
+            <LinkContainer to="/test2">
+              <Nav.Link>test2</Nav.Link>
+            </LinkContainer>
+
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+
+      </Jumbotron>
+    </Container>
+  </MemoryRouter>
+);
+
+export default App;
